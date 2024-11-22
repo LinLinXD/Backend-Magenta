a592daef742814a2dc11037ba13f631b274b1321
Liang Camilo <liangcamilo2006@gmail.com>
2024-11-22 13:34:51 -0500

    feat: addes SSL

--- src/main/java/com/magenta/configuration/SecurityConfig.java
+++ src/main/java/com/magenta/configuration/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:9090"));
+        configuration.setAllowedOrigins(Arrays.asList("https://localhost:9090")); // Actualiza el puerto frontend
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
@@ -89,7 +89,6 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 
 
 }
